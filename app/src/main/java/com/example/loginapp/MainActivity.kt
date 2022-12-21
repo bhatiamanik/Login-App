656904cb38778111a67bbf46b6280480f9bb2c5f
Manik <manbhatia007@gmail.com>
2022-12-21 22:59:55 +0530

    first page done

---
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.loginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+        LoginButton.setOnClickListener {
+            if (checking()) {
+                val email = Email.text.toString()
+                val password = Password.text.toString()
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "login successful", Toast.LENGTH_LONG).show()
+                        } else {
+                            Toast.makeText(this, "Wrong details", Toast.LENGTH_LONG).show()
+
+                        }
+                    }
+
+
+            } else {
+                Toast.makeText(this, "Enter The Details", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun checking(): Boolean {
+        if (Email.text.toString().trim { it <= ' ' }.isNotEmpty()
+            && Password.text.toString().trim { it < ' ' }.isNotEmpty()
+        ) {
+            return true
+        }
+        return false
+
+    }
+}
