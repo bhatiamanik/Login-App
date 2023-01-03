dc45f43561a5ea43bbcd462e64364999975e4aeb
Manik <manbhatia007@gmail.com>
2023-01-03 18:38:58 +0530

    shared pref done

--- app/src/main/java/com/example/loginapp/Logged_in.kt
+++ app/src/main/java/com/example/loginapp/Logged_in.kt
@@ -1,5 +1,7 @@
 package com.example.loginapp
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,28 @@ class Logged_in : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logged_in)
+        val sharedPref=this?.getPreferences(Context.MODE_PRIVATE)?:return
+        val spEmail=sharedPref.getString("Email","1")
+        val email=intent.getStringExtra("email")
+
+
+        if (spEmail=="1")
+        {
+
+            with(sharedPref.edit())
+            {
+                putString("Email", email)
+                apply()
+
+
+            }
+
+        } else
+        {
+            var intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
